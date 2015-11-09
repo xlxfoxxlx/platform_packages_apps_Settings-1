70276481e03ae6b2babc8deaf508869f0e99cd24
Alex Cruz <mazdarider23@gmail.com>
2015-11-09 05:22:16 +0000

    Add Google services tile to settings
    Change-Id: I55b0ef921692bdea0f38a790309e4916e68a5d7a
    
    Conflicts:
    	res/values/du_strings.xml
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1329,7 +1329,17 @@ public class SettingsActivity extends Activity
                             UserManager.DISALLOW_DEBUGGING_FEATURES)) {
                         removeTile = true;
                     }
-                }
+                } else if (id == R.id.google_settings) {
+                    // Embedding into Settings is supported from Google Settings
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.google.android.gms", 0).versionCode >= 1);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
+               }
 
                 if (UserHandle.MU_ENABLED && UserHandle.myUserId() != 0
                         && !ArrayUtils.contains(SETTINGS_FOR_RESTRICTED, id)) {
