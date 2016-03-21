DirtyUnicorns/android_packages_apps_Settings

Navbar: expose Fling and Smarbar settings [1/2]
Change-Id: I0e02cb7cd6ab871f08ef049046ea789b6e15e6eb

src/com/android/settings/SettingsActivity.java
@@ -73,7 +73,9 @@ import com.android.settings.accounts.AccountSyncSettings;
 import com.android.settings.applications.DrawOverlayDetails;
 import com.dirtyunicorns.dutweaks.DirtyTweaks;
 import com.dirtyunicorns.dutweaks.fragments.ExpandedDesktop;
+import com.dirtyunicorns.dutweaks.fragments.FlingSettings;
 import com.dirtyunicorns.dutweaks.fragments.NavbarSettings;
+import com.dirtyunicorns.dutweaks.fragments.SmartbarSettings;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ManageAssist;
@@ -363,7 +365,9 @@ public class SettingsActivity extends Activity
             DirtyTweaks.class.getName(),
             ExpandedDesktop.class.getName(),
             DisplayRotation.class.getName(),
-            NavbarSettings.class.getName()
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName()
     };
 
 

xlxfoxxlx/platform_packages_apps_Settings-1

Conflicts:
	src/com/android/settings/SettingsActivity.java

src/com/android/settings/SettingsActivity.java
@@ -73,7 +73,9 @@ import com.android.settings.accounts.AccountSyncSettings;
 import com.android.settings.aosip.OwlsNestSettings;
 import com.android.settings.applications.DrawOverlayDetails;
 import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
+import com.android.settings.solo.FlingSettings;
 import com.android.settings.solo.NavbarSettings;
+import com.android.settings.solo.SmartbarSettings;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ManageAssist;
@@ -362,8 +364,10 @@ public class SettingsActivity extends Activity
             WriteSettingsDetails.class.getName(),
             OwlsNestSettings.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
-            BlacklistSettings.class.getName()
-            NavbarSettings.class.getName()
+            BlacklistSettings.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName()
     };
 
 