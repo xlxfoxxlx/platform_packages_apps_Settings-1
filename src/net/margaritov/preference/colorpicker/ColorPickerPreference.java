ca8e39f2fd887e46d28dd7966bbde7ce9c1f46bd
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-03-27 03:21:40 +0200

    Color picker: Use a circle shape for the color preview
    Conflicts:
    	res/values/custom_dimens.xml
    	src/net/margaritov/preference/colorpicker/ColorPickerPreference.java

--- src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
+++ src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
@@ -21,9 +21,10 @@ package net.margaritov.preference.colorpicker;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.PorterDuff.Mode;
 import android.os.Bundle;
 import android.preference.DialogPreference;
 import android.preference.Preference;
@@ -206,10 +207,8 @@ public class ColorPickerPreference extends DialogPreference implements
                 widgetFrameView.removeView(preview);
             }
         }
-        widgetFrameView.addView(iView);
+        widgetFrameView.addView(getPreview());
         widgetFrameView.setMinimumWidth(0);
-        iView.setBackgroundDrawable(new AlphaPatternDrawable((int) (5 * mDensity)));
-        iView.setImageBitmap(getPreviewBitmap());
         iView.setTag("preview");
         iView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -219,24 +218,21 @@ public class ColorPickerPreference extends DialogPreference implements
         });
     }
 
-    private Bitmap getPreviewBitmap() {
-        int d = (int) (mDensity * 31); // 30dip
-        int color = getValue();
-        Bitmap bm = Bitmap.createBitmap(d, d, Config.ARGB_8888);
-        int w = bm.getWidth();
-        int h = bm.getHeight();
-        int c = color;
-        for (int i = 0; i < w; i++) {
-            for (int j = i; j < h; j++) {
-                c = (i <= 1 || j <= 1 || i >= w - 2 || j >= h - 2) ? Color.GRAY : color;
-                bm.setPixel(i, j, c);
-                if (i != j) {
-                    bm.setPixel(j, i, c);
-                }
-            }
-        }
+    private ImageView getPreview() {
+        final Resources res = getContext().getResources();
+        final int size = (int) res.getDimension(R.dimen.color_picker_preference_preview_width_height);
 
-        return bm;
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(size, size);
+        LayerDrawable colorPreview = (LayerDrawable) res.getDrawable(
+                R.drawable.color_picker_preference_preview).mutate();
+        ImageView iv = new ImageView(getContext());
+
+        colorPreview.findDrawableByLayerId(R.id.color_fill).setColorFilter(getValue(), Mode.MULTIPLY);
+        iv.setLayoutParams(lp);
+        iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        iv.setImageDrawable(colorPreview);
+
+        return iv;
     }
 
     public int getValue() {
