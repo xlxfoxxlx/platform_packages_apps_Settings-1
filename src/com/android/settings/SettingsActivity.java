ResurrectionRemix/Resurrection_packages_apps_Settings

Update Some Settings Activities
--Add Pulse Settings
--Markiov ColorPicker

Signed-off-by: Varun Date <date.varun123@gmail.com>

src/com/android/settings/SettingsActivity.java
@@ -74,6 +74,7 @@ import com.android.settings.applications.DrawOverlayDetails;
 import com.android.settings.rr.Navbar;
 import com.android.settings.rr.FlingSettings;
 import com.android.settings.rr.SmartbarSettings;
+import com.android.settings.rr.PulseSettings;
 import com.android.settings.headsup.HeadsUpSettings;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
@@ -401,6 +402,7 @@ public class SettingsActivity extends Activity
             DozeSettingsFragment.class.getName(),
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
+            PulseSettings.class.getName(),
             WeatherServiceSettings.class.getName()
     };
 

xlxfoxxlx/platform_packages_apps_Settings-1

Conflicts:
	AndroidManifest.xml
	res/values/rr_strings.xml
	src/com/android/settings/SettingsActivity.java

src/com/android/settings/SettingsActivity.java
@@ -76,6 +76,7 @@ import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.solo.FlingSettings;
 import com.android.settings.solo.NavbarSettings;
 import com.android.settings.solo.SmartbarSettings;
+import com.android.settings.solo.PulseSettings;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ManageAssist;
@@ -368,6 +369,7 @@ public class SettingsActivity extends Activity
             NavbarSettings.class.getName(),
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName()
+            PulseSettings.class.getName(),
     };
 
 