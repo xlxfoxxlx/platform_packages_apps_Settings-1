be626f4b359827047b3f4a38262af8edf1d6daf0
Alex Cruz <mazdarider23@gmail.com>
2016-05-29 16:44:18 -0400

    Add deviceSupportsFlashLight to SettingsUtils

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1220,5 +1220,27 @@ public final class Utils {
         }
         return false;
     }
+
+    public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException e) {
+            // Ignore
+        }
+        return false;
+    }
 }
 
