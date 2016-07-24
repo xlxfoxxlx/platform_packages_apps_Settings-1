DirtyUnicorns/android_packages_apps_Settings

Selinux Switch [2/2]
Change-Id: I64d8d7257374e4af9064ca2bf7a3758f5a49ca5d

src/com/android/settings/du/OnBoot.java
@@ -1,0 +1,51 @@
+package com.android.settings.du;
+
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.util.du.CMDProcessor;
+
+import java.io.IOException;
+import java.util.List;
+
+public class OnBoot extends BroadcastReceiver {
+
+    Context settingsContext = null;
+    private static final String TAG = "SettingsOnBoot";
+    Boolean mSetupRunning = false;
+
+    protected int getMetricsCategory() {
+        return MetricsLogger.DIRTYTWEAKS;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
+        for(int i = 0; i < procInfos.size(); i++)
+        {
+            if(procInfos.get(i).processName.equals("com.google.android.setupwizard")) {
+                mSetupRunning = true;
+            }
+        }
+        if(!mSetupRunning) {
+             try {
+                 settingsContext = context.createPackageContext("com.android.settings", 0);
+             } catch (Exception e) {
+                 Log.e(TAG, "Package not found", e);
+             }
+             SharedPreferences sharedpreferences = settingsContext.getSharedPreferences("com.android.settings_preferences", Context.MODE_PRIVATE);
+             if(sharedpreferences.getBoolean("selinux", true)) {
+                 CMDProcessor.runSuCommand("setenforce 1");
+             } else if (!sharedpreferences.getBoolean("selinux", true)) {
+                 CMDProcessor.runSuCommand("setenforce 0");
+             }
+        }
+    }
+}

xlxfoxxlx/platform_packages_apps_Settings-1

src/com/android/settings/aosip/OnBoot.java
@@ -1,0 +1,51 @@
+package com.android.settings.aosip;
+
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.util.du.CMDProcessor;
+
+import java.io.IOException;
+import java.util.List;
+
+public class OnBoot extends BroadcastReceiver {
+
+    Context settingsContext = null;
+    private static final String TAG = "SettingsOnBoot";
+    Boolean mSetupRunning = false;
+
+    protected int getMetricsCategory() {
+        return MetricsLogger.OWLSNEST;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
+        for(int i = 0; i < procInfos.size(); i++)
+        {
+            if(procInfos.get(i).processName.equals("com.google.android.setupwizard")) {
+                mSetupRunning = true;
+            }
+        }
+        if(!mSetupRunning) {
+             try {
+                 settingsContext = context.createPackageContext("com.android.settings", 0);
+             } catch (Exception e) {
+                 Log.e(TAG, "Package not found", e);
+             }
+             SharedPreferences sharedpreferences = settingsContext.getSharedPreferences("com.android.settings_preferences", Context.MODE_PRIVATE);
+             if(sharedpreferences.getBoolean("selinux", true)) {
+                 CMDProcessor.runSuCommand("setenforce 1");
+             } else if (!sharedpreferences.getBoolean("selinux", true)) {
+                 CMDProcessor.runSuCommand("setenforce 0");
+             }
+        }
+    }
+}