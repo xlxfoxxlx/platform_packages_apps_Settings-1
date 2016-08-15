AOSPA/android_packages_apps_Settings

[2/2]Settings: halo
Change-Id: I31d8b3aa26ae80fccae5abfe6752297ed373223e
Signed-off-by: thecrazyskull <anaskarbila@aospa.co>

src/com/android/settings/paranoid/Halo.java
@@ -1,0 +1,158 @@
+/*
+ * Copyright (C) 2012 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.paranoid;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsLogger;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class Halo extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_HALO_HIDE = "halo_hide";
+    private static final String KEY_HALO_SIZE = "halo_size";
+    private static final String KEY_HALO_PAUSE = "halo_pause";
+    private static final String KEY_HALO_MSGBOX = "halo_msgbox";
+    private static final String KEY_HALO_MSGBOX_ANIMATION = "halo_msgbox_animation";
+    private static final String KEY_HALO_NOTIFY_COUNT = "halo_notify_count";
+    private static final String KEY_HALO_UNLOCK_PING = "halo_unlock_ping";
+
+    private ListPreference mHaloSize;
+    private CheckBoxPreference mHaloHide;
+    private CheckBoxPreference mHaloPause;
+    private ListPreference mHaloNotifyCount;
+    private ListPreference mHaloMsgAnimate;
+    private CheckBoxPreference mHaloMsgBox;
+    private CheckBoxPreference mHaloUnlockPing;
+
+    private Context mContext;
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.HALO_SETTINGS;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.halo_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mContext = getActivity();
+
+        mHaloHide = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_HIDE);
+        mHaloHide.setChecked(Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.HALO_HIDE, 0) == 1);
+
+        int isLowRAM = (!ActivityManager.isLowRamDeviceStatic()) ? 0 : 1;
+        mHaloPause = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_PAUSE);
+        mHaloPause.setChecked(Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.HALO_PAUSE, isLowRAM) == 1);
+
+        mHaloSize = (ListPreference) prefSet.findPreference(KEY_HALO_SIZE);
+        try {
+            float haloSize = Settings.Secure.getFloat(mContext.getContentResolver(),
+                    Settings.Secure.HALO_SIZE, 1.0f);
+            mHaloSize.setValue(String.valueOf(haloSize));
+        } catch(Exception ex) {
+            // So what
+        }
+        mHaloSize.setOnPreferenceChangeListener(this);
+
+        mHaloMsgBox = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_MSGBOX);
+        mHaloMsgBox.setChecked(Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.HALO_MSGBOX, 1) == 1);
+
+        mHaloUnlockPing = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_UNLOCK_PING);
+        mHaloUnlockPing.setChecked(Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.HALO_UNLOCK_PING, 0) == 1);
+
+        mHaloNotifyCount = (ListPreference) prefSet.findPreference(KEY_HALO_NOTIFY_COUNT);
+        try {
+            int haloCounter = Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_NOTIFY_COUNT, 4);
+            mHaloNotifyCount.setValue(String.valueOf(haloCounter));
+        } catch(Exception ex) {
+            // fail...
+        }
+        mHaloNotifyCount.setOnPreferenceChangeListener(this);
+
+        mHaloMsgAnimate = (ListPreference) prefSet.findPreference(KEY_HALO_MSGBOX_ANIMATION);
+        try {
+            int haloMsgAnimation = Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_MSGBOX_ANIMATION, 2);
+            mHaloMsgAnimate.setValue(String.valueOf(haloMsgAnimation));
+        } catch(Exception ex) {
+            // fail...
+        }
+        mHaloMsgAnimate.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mHaloHide) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_HIDE, mHaloHide.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloPause) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_PAUSE, mHaloPause.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloMsgBox) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_MSGBOX, mHaloMsgBox.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloUnlockPing) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_UNLOCK_PING, mHaloUnlockPing.isChecked()
+                    ? 1 : 0);
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHaloSize) {
+            float haloSize = Float.valueOf((String) newValue);
+            Settings.Secure.putFloat(getActivity().getContentResolver(),
+                    Settings.Secure.HALO_SIZE, haloSize);
+            return true;
+        } else if (preference == mHaloMsgAnimate) {
+            int haloMsgAnimation = Integer.valueOf((String) newValue);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.HALO_MSGBOX_ANIMATION, haloMsgAnimation);
+            return true;
+        } else if (preference == mHaloNotifyCount) {
+            int haloNotifyCount = Integer.valueOf((String) newValue);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.HALO_NOTIFY_COUNT, haloNotifyCount);
+            return true;
+        }
+        return false;
+    }
+}

src/com/android/settings/notification/AppNotificationSettings.java
@@ -60,6 +60,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
     private static final String KEY_SENSITIVE = "sensitive";
     private static final String KEY_APP_SETTINGS = "app_settings";
     private static final String KEY_FLOATING = "floating";
+    private static final String KEY_HALO = "halo";
 
     private static final Intent APP_NOTIFICATION_PREFS_CATEGORY_INTENT
             = new Intent(Intent.ACTION_MAIN)
@@ -73,6 +74,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
     private SwitchPreference mPeekable;
     private SwitchPreference mSensitive;
     private SwitchPreference mFloating;
+    private SwitchPreference mHalo;
     private AppRow mAppRow;
     private boolean mCreated;
     private boolean mIsSystemPackage;
@@ -141,6 +143,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
         mPeekable = (SwitchPreference) findPreference(KEY_PEEKABLE);
         mSensitive = (SwitchPreference) findPreference(KEY_SENSITIVE);
         mFloating = (SwitchPreference) findPreference(KEY_FLOATING);
+        mHalo = (SwitchPreference) findPreference(KEY_HALO);
 
         mAppRow = mBackend.loadAppRow(pm, info.applicationInfo);
 
@@ -155,6 +158,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
         mPeekable.setChecked(mAppRow.peekable);
         mSensitive.setChecked(mAppRow.sensitive);
         mFloating.setChecked(mAppRow.floating);
+        mHalo.setChecked(mAppRow.halo);
 
         mBlock.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
             @Override
@@ -192,6 +196,14 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
             public boolean onPreferenceChange(Preference preference, Object newValue) {
                 final boolean floating = (Boolean) newValue;
                 return mBackend.setFloating(mPkg, mUid, floating);
+            }
+        });
+
+        mHalo.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                final boolean halo = (Boolean) newValue;
+                return mBackend.setHalo(mPkg, mUid, halo);
             }
         });
 
@@ -236,6 +248,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
         setVisible(mPriority, mIsSystemPackage || !banned);
         setVisible(mPeekable, mIsSystemPackage || !banned);
         setVisible(mFloating, mIsSystemPackage || !banned);
+        setVisible(mHalo, mIsSystemPackage || !banned);
         setVisible(mSensitive, mIsSystemPackage || !banned && lockscreenSecure
                 && lockscreenNotificationsEnabled && allowPrivate);
     }

xlxfoxxlx/platform_packages_apps_Settings-1

src/com/android/settings/aosip/advanced/Halo.java
@@ -1,0 +1,158 @@
+/*
+ * Copyright (C) 2012 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aosip.advanced;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsLogger;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class Halo extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_HALO_HIDE = "halo_hide";
+    private static final String KEY_HALO_SIZE = "halo_size";
+    private static final String KEY_HALO_PAUSE = "halo_pause";
+    private static final String KEY_HALO_MSGBOX = "halo_msgbox";
+    private static final String KEY_HALO_MSGBOX_ANIMATION = "halo_msgbox_animation";
+    private static final String KEY_HALO_NOTIFY_COUNT = "halo_notify_count";
+    private static final String KEY_HALO_UNLOCK_PING = "halo_unlock_ping";
+
+    private ListPreference mHaloSize;
+    private CheckBoxPreference mHaloHide;
+    private CheckBoxPreference mHaloPause;
+    private ListPreference mHaloNotifyCount;
+    private ListPreference mHaloMsgAnimate;
+    private CheckBoxPreference mHaloMsgBox;
+    private CheckBoxPreference mHaloUnlockPing;
+
+    private Context mContext;
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.OWLSNEST;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.aosip_halo);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mContext = getActivity();
+
+        mHaloHide = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_HIDE);
+        mHaloHide.setChecked(Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.HALO_HIDE, 0) == 1);
+
+        int isLowRAM = (!ActivityManager.isLowRamDeviceStatic()) ? 0 : 1;
+        mHaloPause = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_PAUSE);
+        mHaloPause.setChecked(Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.HALO_PAUSE, isLowRAM) == 1);
+
+        mHaloSize = (ListPreference) prefSet.findPreference(KEY_HALO_SIZE);
+        try {
+            float haloSize = Settings.Secure.getFloat(mContext.getContentResolver(),
+                    Settings.Secure.HALO_SIZE, 1.0f);
+            mHaloSize.setValue(String.valueOf(haloSize));
+        } catch(Exception ex) {
+            // So what
+        }
+        mHaloSize.setOnPreferenceChangeListener(this);
+
+        mHaloMsgBox = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_MSGBOX);
+        mHaloMsgBox.setChecked(Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.HALO_MSGBOX, 1) == 1);
+
+        mHaloUnlockPing = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_UNLOCK_PING);
+        mHaloUnlockPing.setChecked(Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.HALO_UNLOCK_PING, 0) == 1);
+
+        mHaloNotifyCount = (ListPreference) prefSet.findPreference(KEY_HALO_NOTIFY_COUNT);
+        try {
+            int haloCounter = Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_NOTIFY_COUNT, 4);
+            mHaloNotifyCount.setValue(String.valueOf(haloCounter));
+        } catch(Exception ex) {
+            // fail...
+        }
+        mHaloNotifyCount.setOnPreferenceChangeListener(this);
+
+        mHaloMsgAnimate = (ListPreference) prefSet.findPreference(KEY_HALO_MSGBOX_ANIMATION);
+        try {
+            int haloMsgAnimation = Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_MSGBOX_ANIMATION, 2);
+            mHaloMsgAnimate.setValue(String.valueOf(haloMsgAnimation));
+        } catch(Exception ex) {
+            // fail...
+        }
+        mHaloMsgAnimate.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mHaloHide) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_HIDE, mHaloHide.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloPause) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_PAUSE, mHaloPause.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloMsgBox) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_MSGBOX, mHaloMsgBox.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloUnlockPing) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_UNLOCK_PING, mHaloUnlockPing.isChecked()
+                    ? 1 : 0);
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHaloSize) {
+            float haloSize = Float.valueOf((String) newValue);
+            Settings.Secure.putFloat(getActivity().getContentResolver(),
+                    Settings.Secure.HALO_SIZE, haloSize);
+            return true;
+        } else if (preference == mHaloMsgAnimate) {
+            int haloMsgAnimation = Integer.valueOf((String) newValue);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.HALO_MSGBOX_ANIMATION, haloMsgAnimation);
+            return true;
+        } else if (preference == mHaloNotifyCount) {
+            int haloNotifyCount = Integer.valueOf((String) newValue);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.HALO_NOTIFY_COUNT, haloNotifyCount);
+            return true;
+        }
+        return false;
+    }
+}

src/com/android/settings/notification/AppNotificationSettings.java
@@ -59,6 +59,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
     private static final String KEY_PEEKABLE = "peekable";
     private static final String KEY_SENSITIVE = "sensitive";
     private static final String KEY_APP_SETTINGS = "app_settings";
+    private static final String KEY_HALO = "halo";
 
     private static final Intent APP_NOTIFICATION_PREFS_CATEGORY_INTENT
             = new Intent(Intent.ACTION_MAIN)
@@ -71,6 +72,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
     private SwitchPreference mPriority;
     private SwitchPreference mPeekable;
     private SwitchPreference mSensitive;
+    private SwitchPreference mHalo;
     private AppRow mAppRow;
     private boolean mCreated;
     private boolean mIsSystemPackage;
@@ -137,6 +139,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
         mPriority = (SwitchPreference) findPreference(KEY_PRIORITY);
         mPeekable = (SwitchPreference) findPreference(KEY_PEEKABLE);
         mSensitive = (SwitchPreference) findPreference(KEY_SENSITIVE);
+        mHalo = (SwitchPreference) findPreference(KEY_HALO);
 
         mAppRow = mBackend.loadAppRow(pm, info.applicationInfo);
 
@@ -150,6 +153,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
         mPriority.setChecked(mAppRow.priority);
         mPeekable.setChecked(mAppRow.peekable);
         mSensitive.setChecked(mAppRow.sensitive);
+        mHalo.setChecked(mAppRow.halo);
 
         mBlock.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
             @Override
@@ -179,6 +183,14 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
             public boolean onPreferenceChange(Preference preference, Object newValue) {
                 final boolean peekable = (Boolean) newValue;
                 return mBackend.setPeekable(pkg, mUid, peekable);
+            }
+        });
+
+        mHalo.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                final boolean halo = (Boolean) newValue;
+                return mBackend.setHalo(pkg, mUid, halo);
             }
         });
 
@@ -222,6 +234,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
         setVisible(mBlock, !mIsSystemPackage);
         setVisible(mPriority, mIsSystemPackage || !banned);
         setVisible(mPeekable, mIsSystemPackage || !banned);
+        setVisible(mHalo, mIsSystemPackage || !banned);
         setVisible(mSensitive, mIsSystemPackage || !banned && lockscreenSecure
                 && lockscreenNotificationsEnabled && allowPrivate);
     }