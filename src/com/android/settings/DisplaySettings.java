2666fe95e727df9e4ed4c3dc41bf9ed64bf720db
Alex Cruz <mazdarider23@gmail.com>
2017-01-22 09:39:19 +0100

    Rework dashboard settings [1/2]
    - Get rid of the list preference, use seekbar because is sexy af
    - Keep portrait/landscape column count apart
    - Rename strings, no need to say dashboard every time
    - Removed summaries double lines toggle, do it based on the condition
    - Correct summary switch logic
    - Fix integer, now working for themes (tested it on Sublime, thanks @xwasabi)
    
    Change-Id: Ie114174af545fcdeb0331d70c2e4b643f523cc49

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -71,6 +71,8 @@ import com.android.settings.search.Indexable;
 import com.android.settingslib.RestrictedLockUtils;
 import com.android.settingslib.RestrictedPreference;
 
+import com.aosip.owlsnest.preference.CustomSeekBarPreference;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -128,15 +130,17 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String FONT_SIZE_LARGE = "1.15";
     private static final String FONT_SIZE_VERYLARGE = "1.30";
 
-    private static final String DASHBOARD_COLUMNS = "dashboard_columns";
+    private static final String DASHBOARD_PORTRAIT_COLUMNS = "dashboard_portrait_columns";
+    private static final String DASHBOARD_LANDSCAPE_COLUMNS = "dashboard_landscape_columns";
 
     private Preference mFontSizePref;
     private ScreenZoomPreference mScreenZoomPref;
     private WarnedPreference mDialogPref;
 
-    private TimeoutListPreference mScreenTimeoutPreference;
+    private CustomSeekBarPreference mDashboardPortraitColumns;
+    private CustomSeekBarPreference mDashboardLandscapeColumns;
+
     private ListPreference mNightModePreference;
-    private ListPreference mDashboardColumns;
     private Preference mScreenSaverPreference;
     private PreferenceScreen mDisplayRotationPreference;
     private SwitchPreference mLiftToWakePreference;
@@ -146,6 +150,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mCameraGesturePreference;
     private SwitchPreference mCameraDoubleTapPowerGesturePreference;
     private SwitchPreference mNetworkNameDisplayedPreference = null;
+
+    private TimeoutListPreference mScreenTimeoutPreference;
 
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mEditor;
@@ -256,11 +262,17 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             removePreference(KEY_DISPLAY_ROTATION);
         }
 
-        mDashboardColumns = (ListPreference) findPreference(DASHBOARD_COLUMNS);
-        mDashboardColumns.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.DASHBOARD_COLUMNS, DashboardSummary.mNumColumns)));
-        mDashboardColumns.setSummary(mDashboardColumns.getEntry());
-        mDashboardColumns.setOnPreferenceChangeListener(this);
+        mDashboardPortraitColumns = (CustomSeekBarPreference) findPreference(DASHBOARD_PORTRAIT_COLUMNS);
+        int columnsPortrait = Settings.System.getInt(resolver,
+                Settings.System.DASHBOARD_PORTRAIT_COLUMNS, DashboardSummary.mNumColumns);
+        mDashboardPortraitColumns.setValue(columnsPortrait / 1);
+        mDashboardPortraitColumns.setOnPreferenceChangeListener(this);
+
+        mDashboardLandscapeColumns = (CustomSeekBarPreference) findPreference(DASHBOARD_LANDSCAPE_COLUMNS);
+        int columnsLandscape = Settings.System.getInt(resolver,
+                Settings.System.DASHBOARD_LANDSCAPE_COLUMNS, 2);
+        mDashboardLandscapeColumns.setValue(columnsLandscape / 1);
+        mDashboardLandscapeColumns.setOnPreferenceChangeListener(this);
 
         if (isVrDisplayModeAvailable(activity)) {
             DropDownPreference vrDisplayPref =
@@ -621,11 +633,16 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             Settings.Secure.putInt(getContentResolver(), CAMERA_GESTURE_DISABLED,
                     value ? 0 : 1 /* Backwards because setting is for disabling */);
         }
-        if (preference == mDashboardColumns) {
-            Settings.System.putInt(getContentResolver(), Settings.System.DASHBOARD_COLUMNS,
-                    Integer.valueOf((String) objValue));
-            mDashboardColumns.setValue(String.valueOf(objValue));
-            mDashboardColumns.setSummary(mDashboardColumns.getEntry());
+        if (preference == mDashboardPortraitColumns) {
+            int columnsPortrait = (Integer) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.DASHBOARD_PORTRAIT_COLUMNS, columnsPortrait * 1);
+            return true;
+        }
+        if (preference == mDashboardLandscapeColumns) {
+            int columnsLandscape = (Integer) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.DASHBOARD_LANDSCAPE_COLUMNS, columnsLandscape * 1);
             return true;
         }
         if (preference == mNightModePreference) {
