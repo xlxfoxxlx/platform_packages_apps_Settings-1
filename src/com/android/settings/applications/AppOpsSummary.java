8227e08a39ac516d57c95d0349cd5fedf36737bf
Clark Scheff <clark@scheffsblend.com>
2017-02-27 00:55:37 +0100

    Guard against themes without colorAccent defined
    If an applied theme omits the colorAccent attribute in their style
    AppOpsSummary will crash because typedValue.resourceId is 0.
    
    Check if typedValue.resourceId is 0 and if so use switch_accent_color
    for tab indicator color.
    
    Change-Id: I2754a88a50277abb1b685183c0fb2e5b606eb451

--- src/com/android/settings/applications/AppOpsSummary.java
+++ src/com/android/settings/applications/AppOpsSummary.java
@@ -38,7 +38,7 @@ import com.android.settings.R;
 public class AppOpsSummary extends InstrumentedFragment {
     // layout inflater object used to inflate views
     private LayoutInflater mInflater;
-    
+
     private ViewGroup mContentContainer;
     private View mRootView;
     private ViewPager mViewPager;
@@ -117,13 +117,12 @@ public class AppOpsSummary extends InstrumentedFragment {
 
         // HACK - https://code.google.com/p/android/issues/detail?id=213359
         ((ViewPager.LayoutParams)tabs.getLayoutParams()).isDecor = true;
-
         Resources.Theme theme = tabs.getContext().getTheme();
         TypedValue typedValue = new TypedValue();
         theme.resolveAttribute(android.R.attr.colorAccent, typedValue, true);
         final int colorAccent = typedValue.resourceId != 0
                 ? getContext().getColor(typedValue.resourceId)
-                : getContext().getColor(R.color.appops_accent_color);
+                : getContext().getColor(R.color.switch_accent_color);
         tabs.setTabIndicatorColor(colorAccent);
 
         // We have to do this now because PreferenceFrameLayout looks at it
