DirtyUnicorns/android_packages_apps_Settings

Add OmniJaws longpress/tile 'more settings' action
- This is combination of service settings and icon pack because
we want to store the icon pack in a Settings.System store

This is a hidden tile that only accessible via the WeatherTile
- more settings - button

Change-Id: I178ff7eae2e9978b19cda30567b337b07f6a42f7

src/com/android/settings/Settings.java
@@ -170,4 +170,5 @@ public class Settings extends SettingsActivity {
     public static class UpdaterActivity extends SettingsActivity { /* empty */ }
     public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
     public static class DataSaverSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class QuickSettingsSettingsActivity extends SettingsActivity { /* empty */ }
 }

xlxfoxxlx/platform_packages_apps_Settings-1

src/com/android/settings/Settings.java
@@ -180,5 +180,5 @@ public class Settings extends SettingsActivity {
     public static class MagiskActivity extends SettingsActivity { /* empty */ }
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
     public static class SuperUserActivity extends SettingsActivity { /* empty */ }
-
+    public static class TileCategorySettingsActivity extends SettingsActivity { /* empty */ }
 }